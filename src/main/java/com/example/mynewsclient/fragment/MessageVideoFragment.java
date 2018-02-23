e355403ad361108400e4728d475ba8ffd60ae490
xgl2017 <m18621137092_1@163.com>
2018-02-23 14:02:43 +0800

    视频播放失败

--- src/main/java/com/example/mynewsclient/fragment/MessageVideoFragment.java
+++ src/main/java/com/example/mynewsclient/fragment/MessageVideoFragment.java
@@ -1,9 +1,28 @@
 package com.example.mynewsclient.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.VideoView;
 
 import com.example.mynewsclient.R;
 import com.shizhefei.fragment.LazyFragment;
+
+import java.util.Random;
+
+import master.flame.danmaku.controller.DrawHandler;
+import master.flame.danmaku.danmaku.model.BaseDanmaku;
+import master.flame.danmaku.danmaku.model.DanmakuTimer;
+import master.flame.danmaku.danmaku.model.IDanmakus;
+import master.flame.danmaku.danmaku.model.android.DanmakuContext;
+import master.flame.danmaku.danmaku.model.android.Danmakus;
+import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
+import master.flame.danmaku.ui.widget.DanmakuView;
 
 /**
  * Created by HGTXxgl on 2018/2/12.
@@ -14,5 +33,132 @@ public class MessageVideoFragment extends LazyFragment {
     protected void onCreateViewLazy(Bundle savedInstanceState) {
         super.onCreateViewLazy(savedInstanceState);
         setContentView(R.layout.fragment_tabmain_message_video);
+        VideoView videoView = (VideoView) findViewById(R.id.video_view);
+        videoView.setVideoPath(Environment.getExternalStorageDirectory() + "/abc.mp4");
+        videoView.start();
+        danmakuView = (DanmakuView) findViewById(R.id.danmaku_view);
+        danmakuView.enableDanmakuDrawingCache(true);
+        danmakuView.setCallback(new DrawHandler.Callback() {
+            @Override
+            public void prepared() {
+                showDanmaku = true;
+                danmakuView.start();
+                generateSomeDanmaku();
+            }
+
+            @Override
+            public void updateTimer(DanmakuTimer timer) {
+
+            }
+
+            @Override
+            public void danmakuShown(BaseDanmaku danmaku) {
+
+            }
+
+            @Override
+            public void drawingFinished() {
+
+            }
+        });
+        danmakuContext = DanmakuContext.create();
+        danmakuView.prepare(parser, danmakuContext);
+
+        final LinearLayout operationLayout = (LinearLayout) findViewById(R.id.operation_layout);
+        final Button send = (Button) findViewById(R.id.send);
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
+        danmakuView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (operationLayout.getVisibility() == View.GONE) {
+                    operationLayout.setVisibility(View.VISIBLE);
+                } else {
+                    operationLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String content = editText.getText().toString();
+                if (!TextUtils.isEmpty(content)) {
+                    addDanmaku(content, true);
+                    editText.setText("");
+                }
+            }
+        });
+        getActivity().getWindow().getDecorView().setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if (visibility == View.SYSTEM_UI_FLAG_VISIBLE) {
+                    getActivity().onWindowFocusChanged(true);
+                }
+            }
+        });
     }
+
+
+
+    private boolean showDanmaku;
+
+    private DanmakuView danmakuView;
+
+    private DanmakuContext danmakuContext;
+
+    private BaseDanmakuParser parser = new BaseDanmakuParser() {
+        @Override
+        protected IDanmakus parse() {
+            return new Danmakus();
+        }
+    };
+
+    /**
+     * 向弹幕View中添加一条弹幕
+     * @param content
+     *          弹幕的具体内容
+     * @param  withBorder
+     *          弹幕是否有边框
+     */
+    private void addDanmaku(String content, boolean withBorder) {
+        BaseDanmaku danmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
+        danmaku.text = content;
+        danmaku.padding = 5;
+        danmaku.textSize = sp2px(20);
+        danmaku.textColor = Color.WHITE;
+        danmaku.setTime(danmakuView.getCurrentTime());
+        if (withBorder) {
+            danmaku.borderColor = Color.GREEN;
+        }
+        danmakuView.addDanmaku(danmaku);
+    }
+
+    /**
+     * 随机生成一些弹幕内容以供测试
+     */
+    private void generateSomeDanmaku() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(showDanmaku) {
+                    int time = new Random().nextInt(300);
+                    String content = "一生挚爱" + time + time;
+                    addDanmaku(content, false);
+                    try {
+                        Thread.sleep(time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * sp转px的方法。
+     */
+    public int sp2px(float spValue) {
+        final float fontScale = getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
 }
